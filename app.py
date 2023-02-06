26c5f4d8a60b61cc05d11c818cf4d9a7a04839a0
Suraj <surajkumar5454@gmail.com>
2023-02-06 15:00:23 +0530

    Added date logic

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import json
+from datetime import datetime
 
 import requests
 from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'  # Replace with a secret key of your own
-
 
 
 def populatenews(topic):
@@ -19,6 +19,22 @@ def populatenews(topic):
     return data["articles"]
 
 
+def populatenewswithdate(topic, datepicked):
+    # Your API key
+    api_key = '20157af1d27548e38a2925331bf1c2d3'
+    response = requests.get('https://newsapi.org/v2/everything?q=' + topic + '&apiKey=' + api_key)
+    filteredData = []
+    # Get the JSON data from the response
+    data = json.loads(response.text)
+    for article in data['articles']:
+        date_object = datetime.strptime(article['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
+        new_date_string = date_object.strftime("%Y-%m-%d")
+        if datepicked == new_date_string:
+            filteredData.append(article)
+    print(data)
+    return filteredData
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
@@ -28,7 +44,12 @@ def index():
 def result():
     data = []
     topic = request.form['newsstring']
-    data = populatenews(topic)
+    datepicked = request.form['datepicker']
+    print("date: " + datepicked)
+    if datepicked == "":
+        data = populatenews(topic)
+    else:
+        data = populatenewswithdate(topic, datepicked)
     return render_template("result.html", data=data)
 
 
